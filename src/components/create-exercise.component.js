b2fbb8a527186cca29ed485a0588be50e63dead2
Benson Njuguna K <bennspencer@gmail.com>
2020-08-01 12:51:16 +0300

    Component creation and initialization

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+         
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        CreateExercise
+      </div>
+    )
+  }
+}
---
+++ src/components/edit-exercise.component.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class EditExercises extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+         
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        EditExercises
+      </div>
+    )
+  }
+}
