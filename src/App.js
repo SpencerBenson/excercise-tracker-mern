8fbc35916774cd3dcbc2af219996d42332dbda4a
Benson Njuguna K <bennspencer@gmail.com>
2020-08-01 16:32:49 +0300

    clean up

--- src/App.js
+++ src/App.js
@@ -10,13 +10,15 @@ import CreateUser from "./components/create-user.component";
 function App() {
   return (
     <Router>
+
       <Navbar />
-      this goes untouched
-      <br />
-      <Route path='/' exact component={ExercisesList} />
-      <Route path='/edit/:id' component={EditExercise} />
-      <Route path='/create' component={CreateExercise} />
-      <Route path='/user' component={CreateUser} />
+      <div className='container'>
+        <br />
+        <Route path='/' exact component={ExercisesList} />
+        <Route path='/edit/:id' component={EditExercise} />
+        <Route path='/create' component={CreateExercise} />
+        <Route path='/user' component={CreateUser} />
+      </div>
 
     </Router>
   );
